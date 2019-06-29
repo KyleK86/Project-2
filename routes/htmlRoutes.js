95304d7957909ca59885241293a651c585012d56
Kyle Knox <kyleknox86@gmail.com>
2019-06-28 19:05:42 -0600

    working routes with commented out code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,36 +1,31 @@
-module.exports = function (app) {
-    // Load index page
-    app.get("/", function (req, res) {
-        db.Robogatchi.findAll({}).then(function (Characters) {
-            res.render("index", {
-                msg: "Welcome!",
-                Robogatchis: Characters
-            });
-        });
-    });
+var path = require("path");
 
+var db = require("../models");
+
+module.exports = function(app) {
+  
     // loads login.html
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/login.html"));
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/login.html"));
     });
 
     // loads signup.html
-    app.get("/signup", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    app.get("/signup", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/signup.html"));
     });
 
     // loads create.html
-    app.get("/create", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/create.html"));
+    app.get("/create", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/create.html"));
     });
 
     // loads main.html
-    app.get("/main", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/main.html"));
+    app.get("/main", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/main.html"));
     });
 
     // Render 404 page for any unmatched routes
-    app.get("*", function (req, res) {
+    app.get("*", function(req, res) {
         res.render("404");
     });
 };
