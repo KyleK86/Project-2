120a0c5eacf9b5988570853bc5f27e802df0a7bc
Amanda Dovel <amandacdovel@gmail.com>
2019-07-01 17:51:48 -0600

    trying to switch branches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.post("/login", passport.authenticate("local"), function(req, res) {
+    app.post("/login", passport.authenticate("local"), function (req, res) {
         res.json(req.user);
     });
 
     // Create a new example
-    app.post("/signup", function(req, res) {
+    app.post("/signup", function (req, res) {
 
         console.log(req.body.username);
         console.log(req.body.email);
@@ -18,17 +18,16 @@ module.exports = function(app) {
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
-        }).then(function() {
+        }).then(function () {
             res.redirect(307, "/login");
-        }).catch(function(err) {
+        }).catch(function (err) {
             res.status(401).json(err);
         });
     });
 
-    // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample);
-        });
+    // post 
+    app.post("/", function (req, res) {
+        res.send(req.body);
+        console.log(req.user);
     });
 };
