882f0c555b244f71515b01a625145abeed32c207
Kyle Knox <kyleknox86@gmail.com>
2019-07-03 22:19:54 -0600

    fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -43,20 +43,21 @@ module.exports = function (app) {
         req.logout();
         res.redirect("/login");
     });
-};
 
-app.put("/api/gotchi/:id", function(req, res) {
-    if (!req.user) {
-        res.redirect("/login");
-    } else {
-        db.gotchi.update(
-            req.body,
-            {
-                where: {
-                    UserId: req.params.id
-                }
-            }).then(function(gotchi) {
-            res.json(gotchi);
-        });
-    }
-});
+
+    app.put("/api/gotchi/:id", function(req, res) {
+        if (!req.user) {
+            res.redirect("/login");
+        } else {
+            db.gotchi.update(
+                req.body,
+                {
+                    where: {
+                        UserId: req.params.id
+                    }
+                }).then(function(gotchi) {
+                res.json(gotchi);
+            });
+        }
+    });
+};
