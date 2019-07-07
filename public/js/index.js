49d2b98a25b2cff5e96be6c5e990bde15ffa760a
andy_hardy <aohardy@gmail.com>
2019-07-07 14:44:29 -0600

    updated math to reset attribute levels and increment health

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,70 @@
 $(document).ready(function () {
 
-    $(".action-btn").on("click", function (event) {
-        var payload = {
-            updatedAt: moment().format("YYYY-MM-DD hh:mm:ss")
-        };
-        var name = event.target.name;
-        var value = parseInt(event.target.value);
-        var id = $(this).data("id");
-
-        $.get("/api/gotchi/" + id, function (res) {
-            var points = res[name];
-            console.log(res[name]);
-            payload[name] = points - value;
-            $.ajax({
-                method: "PUT",
-                url: "/api/gotchi/" + id,
-                data: payload
-
-            }).then(function (response) {
-                console.log(points + value);
-                console.log(response);
-                console.log(name);
-                if (response) {
-                    $("#" + name + "-points").text(points + value);
-                }
-            });
-        });
-
+    $(".action-btn").on("click", function () {
+        var button = $(this);
+        var currentValue = button.data("points");
+        var id = button.data("id");
+        var column = button.data("column");
+        var health = button.data("health");
+        gotchiUpdate(column, health, currentValue, id);
+        modalUpdate(column, health, currentValue);
     });
+
+    $(".modal-footer button").on("click", function() {
+        window.location.reload();
+    });
+
+    function gotchiData(column, health, value) {
+        switch (column) {
+        case "hungry":
+            var data = {
+                hungry: 0,
+                health: health + value
+            };
+            return data;
+        case "bored":
+            var data = {
+                bored: 0,
+                health: health + value
+            };
+            return data;
+        case "lazy":
+            var data = {
+                lazy: 0,
+                health: health + value
+            };
+            return data;
+        }
+    }
+
+    function gotchiUpdate(column, health, value, id) {
+        var data = gotchiData(column, health, value);
+        $.ajax({
+            method: "PUT",
+            url: "/api/gotchi/" + id,
+            data: data
+        }).then(function(data) {
+            console.log(data);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+
+    function modalUpdate(column, health, value) {
+        var newHealth = 0;
+        switch(column) {
+        case "hungry":
+            newHealth = health + value;
+            $("#hungry-points").text(newHealth);
+            break;
+        case "bored":
+            newHealth = health + value;
+            $("#bored-points").text(newHealth);
+            break;
+        case "lazy":
+            newHealth = health + value;
+            $("#lazy-points").text(newHealth);
+            break;
+        }
+    }
 });
